440ec84d58a08af8795650a473e5a434450a99c1
AlastairP <alastairp@jacob_paul@outlook.com>
2018-07-26 23:32:55 +0200

    Added project

---
+++ WebNode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using SharpDB;
+
+namespace WebNode
+{
+    public class Program
+    {
+		static DB dB = new DB(Environment.CurrentDirectory);
+		static void Main(string[] args){
+			if (!Directory.Exists("db")) {
+				Console.WriteLine("No connection found");
+			}
+		}
+		static void Connect(){
+			
+		}
+    }
+}
